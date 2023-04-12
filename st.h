d8b8bb984d453f0a6abd29ed49b249906abc16c3
John <johnmichaelclasson@gmail.com>
2023-04-12 19:31:45 +1000

    removed + added patches

--- st.h
+++ st.h
@@ -37,12 +37,6 @@ enum glyph_attribute {
 	ATTR_BOLD_FAINT = ATTR_BOLD | ATTR_FAINT,
 };
 
-enum drawing_mode {
-    DRAW_NONE = 0,
-    DRAW_BG = 1 << 0,
-    DRAW_FG = 1 << 1,
-};
-
 enum selection_mode {
 	SEL_IDLE = 0,
 	SEL_EMPTY = 1,
@@ -88,8 +82,8 @@ void die(const char *, ...);
 void redraw(void);
 void draw(void);
 
-void opencopied(const Arg *);
 void newterm(const Arg *);
+void opencopied(const Arg *);
 void printscreen(const Arg *);
 void printsel(const Arg *);
 void sendbreak(const Arg *);
