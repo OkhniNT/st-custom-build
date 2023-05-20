c43cf4c4846f750ecb660863b0a87cda78ce12aa
John <johnmichaelclasson@gmail.com>
2023-05-20 15:36:04 +1000

    general updates

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=17:bold:antialias=true:autohint=true";
+static char *font = "monospace:pixelsize=17:antialias=true:autohint=true";
 static int borderpx = 15;
 
 /*
@@ -27,7 +27,7 @@ char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
-static float chscale = 1.2;
+static float chscale = 1.0;
 
 /*
  * word delimiter string
